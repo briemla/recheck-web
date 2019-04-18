62f6e182e5fd26a5ae05ee9fde1826202fd173e6
Jeremias Roessler <roessler@retest.de>
2019-04-18 21:07:22 +0200

    Extend RecheckOptions after release of recheck v.1.1.0

--- src/main/java/de/retest/web/selenium/AutocheckingRecheckDriver.java
+++ src/main/java/de/retest/web/selenium/AutocheckingRecheckDriver.java
@@ -27,13 +27,10 @@ public class AutocheckingRecheckDriver extends RecheckDriver {
 		options = RecheckOptions.builder().build();
 	}
 
-	// TODO Use RecheckWebOptions
-	public AutocheckingRecheckDriver( final RemoteWebDriver wrapped, final RecheckOptions options,
-			final AutocheckingCheckNamingStrategy namingStrategy ) {
+	public AutocheckingRecheckDriver( final RemoteWebDriver wrapped, final RecheckOptions options ) {
 		super( wrapped );
 		this.options = options;
-		// TODO Incorporate AutocheckingCheckNamingStrategy into RecheckWebOptions
-		this.namingStrategy = namingStrategy; // options.getNamingStrategy();
+		namingStrategy = options.getNamingStrategy();
 	}
 
 	public void startTest() {
